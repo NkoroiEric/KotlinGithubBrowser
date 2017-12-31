d9e714ee0d44b5b4ceb407688c1513fd95e029c3
nkoroi <nkoroieric@gmail.com>
2017-12-31 15:26:40 +0000

    every journey begins with a single step and that's gradle setup

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="c0a8d7ac-ea74-4fd2-bc79-1775d84c7e6e")
+public class DataBindingInfo {}
